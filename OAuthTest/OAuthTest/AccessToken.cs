73a953001bb926ba70aee55dbbfb7388f3a7dc2a
Volodymyr Tsurko <sonicpro@ukrpost.ua>
2019-06-06 09:54:27 +0300

    OAuthTest demo for client_credentials Authorisation Grant.

---
+++ OAuthTest/OAuthTest/AccessToken.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OAuthTest
+{
+	class AccessToken
+	{
+		public string access_token { get; set; }
+
+		public string token_type { get; set; }
+
+		public long expires_in { get; set; }
+
+		public string scope { get; set; }
+	}
+}
---
+++ OAuthTest/OAuthTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace OAuthTest
+{
+	class Program
+	{
+		private const string AuthGrant = "grant_type";
+		private const string GrantType = "client_credentials";
+		private const string ClientId = "MartinsburgMonster";
+		private const string ClientSecret = "bb3ab7975429748834c28686d3886840";
+
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Coop API (http://http://coop.apps.symfonycasts.com).");
+			AccessToken token = AcquireTokenAsync().Result;
+			Console.WriteLine($"AccessToken: {token.access_token}");
+		}
+
+
+		#region Helper Methods
+
+		private static async Task<AccessToken> AcquireTokenAsync()
+		{
+			using (var client = new HttpClient())
+			{
+				// Define headers
+				client.DefaultRequestHeaders.Accept.Clear();
+				client.DefaultRequestHeaders.Accept.Add(
+					new MediaTypeWithQualityHeaderValue("application/json"));
+
+				var content = GetBody(AuthGrant, GrantType, ClientId, ClientSecret);
+				var response = await client.PostAsync("http://coop.apps.knpuniversity.com/token", content);
+				var messageString = await response.Content.ReadAsStringAsync();
+				return JsonConvert.DeserializeObject<AccessToken>(messageString);
+			}
+		}
+
+		private static FormUrlEncodedContent GetBody(string authGrant,
+			string grantType,
+			string clientId,
+			string clientSecret)
+		{
+			var formData = new List<KeyValuePair<string, string>>();
+			formData.Add(new KeyValuePair<string, string>("client_id", clientId));
+			formData.Add(new KeyValuePair<string, string>("client_secret", clientSecret));
+			formData.Add(new KeyValuePair<string, string>(authGrant, grantType));
+
+			return new FormUrlEncodedContent(formData);
+		}
+
+		#endregion
+	}
+}
