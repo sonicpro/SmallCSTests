790ffbec429784d72ac2e1dc14f72d99a5fbb0c0
Volodymyr Tsurko <chegnenado@yahoo.com>
2020-03-20 11:40:23 +0200

    Added "BoxingWhenCastingToAnInterface" demo.

---
+++ BoxingWhenCastingToAnInterface/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BoxingWhenCastingToAnInterface
+{
+    public class Class1
+    {
+        double someMoney = 4.0;
+
+        IComparable comparableMoney;
+
+        public Class1()
+        {
+            comparableMoney = this.someMoney;
+        }
+
+        public IComparable ComparableMoney => comparableMoney;
+    }
+}
