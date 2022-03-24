289112ae8eb057cd8f7bf1e89726481f8a9e899d
Volodymyr Tsurko <volodymyr.tsurko@externals.hilti.com>
2022-03-24 13:57:32 +0200

    Added Cancelled Delay Task demo.

---
+++ CancellingTaskDelay/MessagesHolder.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellingTaskDelay
+{
+    public class MessagesHolder<T>
+    {
+        private readonly ConcurrentDictionary<string, T> dict = new();
+
+        public void Add(string key, T item)
+        {
+            this.dict.TryAdd(key, item);
+        }
+
+        public async Task<IEnumerable<T>> RequestMessages(CancellationToken cancellationToken)
+        {
+            var list = new List<T>();
+            while (this.dict.IsEmpty)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    break;
+                }
+
+                await Task.Delay(50, cancellationToken);
+            }
+
+            foreach (var pair in this.dict)
+            {
+                list.Add(pair.Value);
+                this.dict.TryRemove(pair);
+            }
+
+            return list;
+        }
+    }
+}
---
+++ CancellingTaskDelay/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CancellingTaskDelay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cts = new CancellationTokenSource();
+            var holder = new MessagesHolder<string>();
+            var t = Task.Run(async delegate
+            {
+                await holder.RequestMessages(cts.Token);
+            });
+            cts.Cancel();
+            t.Wait();
+        }
+    }
+}
